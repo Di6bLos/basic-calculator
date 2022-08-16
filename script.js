4be042597e4d1c00d372e5a1d52af90711a20839
CarlosPrieto <carlosprieto66@icloud.com>
2022-08-16 10:45:11 -0700

    funtion that takes 2 numbers and an operator

--- script.js
+++ script.js
@@ -11,3 +11,24 @@ const multiply = (x,y) => x * y;
 const divide = (x,y) => x / y;
 
 
+// Operator function
+
+const displayValue = [];
+let num1 = 0;
+let num2 = 0;
+
+
+const operate = (x, func, y) => {
+
+    if (func === add) {
+        return  displayValue.push(add(x, y));
+    } else if (func === subtract) {
+        return  displayValue.push(subtract(x, y));
+    } else if (func === multiply) {
+        return  displayValue.push(multiply(x, y));
+    } else if (func === divide) {
+        return  displayValue.push(divide(x, y));
+    };
+
+};
+
