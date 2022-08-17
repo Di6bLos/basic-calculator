4663ef9aa2b4c1253d291dbdfab70a3faa5858d0
CarlosPrieto <carlosprieto66@icloud.com>
2022-08-16 21:30:17 -0700

    operate function displays results

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 "use strict"
+const buttons = document.querySelectorAll('.btn');
+const displayScreen = document.querySelector('.display');
+const displayValue = [];
+let num1 = 0;
+let num2 = 0;
+
 
 // Basic arithmatic functions
 
@@ -13,33 +19,32 @@ const divide = (x,y) => x / y;
 
 // Operator function
 
-const displayValue = [];
-let num1 = 0;
-let num2 = 0;
-
-
 const operate = (x, func, y) => {
 
     if (func === add) {
+        displayScreen.innerText = displayValue;
         return  displayValue.push(add(x, y));
     } else if (func === subtract) {
+        displayScreen.innerText = displayValue;
         return  displayValue.push(subtract(x, y));
     } else if (func === multiply) {
+        displayScreen.innerText = displayValue;
         return  displayValue.push(multiply(x, y));
     } else if (func === divide) {
+        displayScreen.innerText = displayValue;
         return  displayValue.push(divide(x, y));
     };
 
 };
 
-const displayScreen = document.querySelector('.display');
 
-const buttons = document.querySelectorAll('.btn');
+// Displaying the value of the buttons pressed.
 
-buttons.forEach(button => button.addEventListener('click', function(e) {
-
-    const value = e.target.value;
+const returnValue = (e) => {
+        const value = e.target.value;
     displayValue.push(value);
     displayScreen.innerText = displayValue.join('');
-}));
+};
+
+buttons.forEach(button => button.addEventListener('click', returnValue));
 
