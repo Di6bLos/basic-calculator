e7a04335fdffd336494fe53a2cfdd1bc6db91dbf
CarlosPrieto <carlosprieto66@icloud.com>
2022-08-23 19:26:52 -0700

    numbers and operations appear on screen and are assigned to variables

--- script.js
+++ script.js
@@ -19,3 +19,31 @@ const subtract = (x,y) => x - y;
 const multiply = (x,y) => x * y;
 const divide = (x,y) => x / y;
 
+// Numbers added to screen and variables to be calculated
+numberBtns.forEach((btn)=> btn.addEventListener('click', (e)=> {
+    // Checks if a decimal is already used
+    if (e.target.innerText === '.' && haveDot === false) {
+        haveDot = true;
+    } else if (e.target.innerText === '.' && haveDot === true) {
+        return;
+    }
+    // Checks if operation has been selected and assigns number variables.
+    if (operation === '') {
+          num1 += e.target.innerText;
+    display.innerText = num1;
+    } else if (operation != '') {
+        num2 += e.target.innerText;
+        display.innerText = num1 + operation + num2;
+    }
+
+}));
+
+// Operations variable selected
+operateBtns.forEach(op => op.addEventListener('click', (e)=> {
+    if (operation === '') {
+        operation = e.target.innerText;
+        display.innerText += e.target.innerText;
+    } else if (operation != '') {
+        operate();
+    }
+}));
