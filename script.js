9fb023f3ac25e86048717db24801f0cccc71a40c
CarlosPrieto <carlosprieto66@icloud.com>
2022-08-23 17:40:57 -0700

    created dom elements

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-"use"
+"use strict"
+
+// DOM elements
+const numberBtns = document.querySelectorAll('.number');
+const operateBtns = document.querySelectorAll('.operation');
+const clearBtn = document.getElementById('btnClearAll');
+const deleteBtn = document.getElementById('btnDelete');
+const equalBtn = document.getElementById('btnEqual');
+const dotBtn = document.getElementById('btnDeci');
+const displayValue = document.getElementById('display').innerText;
+
