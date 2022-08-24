158493e9ad54c5df4812ef9bf1863a6643aa62c4
CarlosPrieto <carlosprieto66@icloud.com>
2022-08-23 19:24:44 -0700

    basic math functions

--- script.js
+++ script.js
@@ -7,5 +7,15 @@ const clearBtn = document.getElementById('btnClearAll');
 const deleteBtn = document.getElementById('btnDelete');
 const equalBtn = document.getElementById('btnEqual');
 const dotBtn = document.getElementById('btnDeci');
-const displayValue = document.getElementById('display').innerText;
+const display = document.getElementById('display');
+let num1 = '';
+let num2 = '';
+let operation = '';
+let haveDot = false;
+
+// Math functions
+const add = (x,y) => x + y;
+const subtract = (x,y) => x - y;
+const multiply = (x,y) => x * y;
+const divide = (x,y) => x / y;
 
